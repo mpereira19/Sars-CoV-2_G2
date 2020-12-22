547b75b12d322f4a2b26561de571a286e54eb3b2
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2020-12-22 15:36:52 +0000

    Update NCBI.py

--- NCBI.py
+++ NCBI.py
@@ -57,14 +57,16 @@ print('tamanho da sequência:', len(record.seq))
 print('referências a bases de dados (externas):', record.dbxrefs)
 print('Número de anotações:', len(record.annotations))
 print('Fonte de anotação:', record.annotations["source"] )
-print('Record Annotation:' , record.letter_annotations)
+
 print('Número de features', len(record.features))
 print('Taxonomia:', *record.annotations['taxonomy'])
 print('Organismo:', record.annotations['organism'])
 
 featregion = [i for i in range(len(record.features)) if record.features[i].type=='Region']
-seqs_location = [print (record.features[k].location) for k in featregion]
-seqs_features = [print(record.features[k].extract(record.seq)) for k in featregion]
+
+for  k in featregion:
+    print (record.features[k].location)
+    print(record.features[k].extract(record.seq))
 
 
 #   MAVS
@@ -77,14 +79,16 @@ print('tamanho da sequência:', len(record1.seq))
 print('referências a bases de dados (externas):', record1.dbxrefs)
 print('Número de anotações:', len(record1.annotations))
 print('Fonte de anotação:', record1.annotations["source"] )
-print(record1.letter_annotations)
+
 print('Número de features', len(record1.features))
 print('Taxonomia:', *record1.annotations['taxonomy'])
 print('Organismo:', record1.annotations['organism'])
 
 featregion1 = [i for i in range(len(record1.features)) if record1.features[i].type=='Region']
-seqs_location1 = [print (record1.features[k].location) for k in featregion1]
-seqs_features1 = [print(record1.features[k].extract(record1.seq)) for k in featregion1]
+
+for k in featregion1:
+    print (record1.features[k].location)
+    print(record1.features[k].extract(record1.seq))
 
 
 #   ORF9b
@@ -97,14 +101,17 @@ print('tamanho da sequência:', len(record2.seq))
 print('referências a bases de dados (externas):', record2.dbxrefs)
 print('Número de anotações:', len(record2.annotations))
 print('Fonte de anotação:', record2.annotations["source"] )
-print(record2.letter_annotations)
+
 print('Número de features', len(record2.features))
 print('Taxonomia:', *record2.annotations['taxonomy'])
 print('Organismo:', record2.annotations['organism'])
 
 featregion2 = [i for i in range(len(record2.features)) if record2.features[i].type=='Region']
-seqs_location2 = [print (record2.features[k].location) for k in featregion2]
-seqs_features2 = [print(record2.features[k].extract(record2.seq)) for k in featregion2]
+
+for k in featregion2:
+    print (record2.features[k].location)
+    print(record2.features[k].extract(record2.seq))
+    print(record2.features[k].sub_features)
 
 
 #Fazer os BLASTs!
