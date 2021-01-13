cfdc4c469c44820206c97003a3e85bee81784ec1
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2021-01-13 17:37:01 +0000

    Update 4_Multiple_alignment.py

--- Protein/4_Multiple_alignment.py
+++ Protein/4_Multiple_alignment.py
@@ -8,8 +8,6 @@ orfs = ['TRAF3', 'MAVS', 'ORF9b']
 # ficheiros '..._alignment.clustal_num' e '..._tree.dnd' foram obtidos através de Clustal Omega online!
 
 for orf in orfs:
-    filename = orf + '_alignment.aln'
-    format = 'clustal'
     align = AlignIO.read(orf + '_alignment.clustal_num', 'clustal')
     print(align)
 
